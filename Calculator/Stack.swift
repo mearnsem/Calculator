dc22c1e9c163c21c848d9a007d79f479ee24e0ef
Emily Mearns <mearnsem@gmail.com>
2016-05-30 17:21:37 -0600

    Stack file setup
    Not sure if it is correct though?

---
+++ Calculator/Stack.swift
@@ -0,0 +1,30 @@
+//
+//  Stack.swift
+//  Calculator
+//
+//  Created by Emily Mearns on 5/30/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class Stack {
+    
+    private var numbers = [Float]()
+    
+    func pop(number: Float) -> Float? {
+        return numbers.removeLast()
+    }
+    
+    func push(number: Float) {
+        numbers.append(number)
+    }
+    
+    func log() {
+        print(numbers)
+    }
+    
+    func count() -> Int {
+        return numbers.count
+    }
+}
